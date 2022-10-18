aa11947a951dfc3589ba7445c95ebc26ad70bc60
서봉진 <seobongjin@MacBook-Pro-2.local>
2022-10-18 16:55:13 +0900

    1018 react comment 만들기

---
+++ src/Comment/CommentList.jsx
@@ -0,0 +1,13 @@
+import Comment from "./Comment";
+
+function CommentList(props){
+
+    return (
+        <div>
+            <Comment name="하상우" comment="안녕하세요"/>
+            <h1>여기는 CommentList 입니다</h1>
+        </div>
+    )
+}
+
+export default CommentList;
